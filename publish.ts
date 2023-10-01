e7183df58ec5e322574a107e0eb3b045c597068e
istarwyh <yihui.wang@pku.edu.cn>
2023-10-01 15:47:01 +0800

    modify

--- publish.ts
+++ publish.ts
@@ -1,4 +1,4 @@
-const { color, log } = require('console-log-colors');
+const {log} = require('console-log-colors');
 const simpleGit = require('simple-git')
 
 const path = 'dist'
@@ -7,16 +7,16 @@ const repo = 'https://github.com/istarwyh/resume-it.git'
 log.green(`Start public to your git repo:${repo}\nPlease wait ...`)
 
 try {
-  const git = simpleGit(path).init().addRemote('origin', repo);
-  git.add('./*')
-  .commit("automatically publish")
-  .checkoutLocalBranch('gh-pages', () => {
-    console.log('Checkout to branch gh-pages');
-  })
-  .push(['-f', 'origin', 'gh-pages'])
-  .checkout('master', () => {
-    log.green('Finish publish, back to branch master.');
-  })
+    const git = simpleGit(path).init().addRemote('origin', repo);
+    git.add('./*')
+        .commit("automatically publish")
+        .checkoutLocalBranch('gh-pages', () => {
+            console.log('Checkout to branch gh-pages');
+        })
+        .push(['-f', 'origin', 'gh-pages'])
+        .checkout('master', () => {
+            log.green('Finish publish, back to branch master.');
+        })
 } catch (error) {
-  log.red(error);
+    log.red(error);
 }
